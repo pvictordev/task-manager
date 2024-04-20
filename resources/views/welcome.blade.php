91e469418c62a54477ac9c8d40aea34052e0dd10
Victor Purice <victor.purice@ulbsibiu.ro>
2024-04-21 00:18:44 +0300

    added auth feature using breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -835,10 +835,23 @@
         <div class="container mx-auto flex items-center justify-between">
             <!-- Logo -->
             <a href="/" class="text-white text-xl font-bold">Task Manager</a>
-
             <!-- Navigation Links -->
-            <div class="flex space-x-4">
+            <div class="flex items-center space-x-4">
                 <a href="/markedRoute" class="text-white hover:text-gray-300">Marked</a>
+                <!-- <a href="/markedRoute" class="text-white hover:text-gray-300">Profile</a> -->
+                @if (Route::has('login'))
+                <div class="p-6 text-right z-10">
+                    @auth
+                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Profile</a>
+                    @else
+                    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                    @endif
+                    @endauth
+                </div>
+                @endif
             </div>
         </div>
     </nav>
